404222b8643588685c88bf609a0827f3cc18b4ec
Guillaume Teillais <guillaume.teillais@gmail.com>
2024-05-15 14:52:55 +0200

    adding ux peugeot slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -270,6 +270,15 @@ window.addEventListener('scroll', () => {
   addClassOnScroll('animscroll');
 });
 
+// before after slider
+$("#slider").on("input change", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $('.foreground-img').css('width', `${sliderPos}%`)
+  // Update the position of the slider button
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+});
+
 // $('.carousel').carousel({
 //   interval: 2000
 // });
